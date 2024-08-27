731f3b8fbc114a9019bd47c711277abbf351f208
Nong Hoang Vu <vunhph33506@fpt.edu.vn>
2024-08-27 13:19:22 +0700

    spring boot 3 - java 21

---
+++ src/main/java/com/javatech/ApiSampleCodeApplication.java
@@ -0,0 +1,13 @@
+package com.javatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSampleCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSampleCodeApplication.class, args);
+    }
+
+}
